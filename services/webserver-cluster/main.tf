93fc4d40fa3b7ace82cfa118931d250c1f683804
Artur Propp <artur.propp@me.com>
2020-09-20 23:22:41 +0200

    fix autoscaling group input/variable name

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -177,7 +177,7 @@ resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
     desired_capacity = 10
     recurrence = "0 9 * * *"
 
-    autoscaling_group_name = module.webserver_cluster.asg_name
+    autoscaling_group_name = aws_autoscaling_group.example.name
 }
 
 resource "aws_autoscaling_schedule" "scale_in_at_night" {
@@ -189,5 +189,5 @@ resource "aws_autoscaling_schedule" "scale_in_at_night" {
     desired_capacity = 2
     recurrence = "0 17 * * *"
 
-    autoscaling_group_name = module.webserver_cluster.asg_name
+    autoscaling_group_name = aws_autoscaling_group.example.name
 }
